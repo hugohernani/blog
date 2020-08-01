663989750353fd589ae4bdf18ded30a1706d1297
Hugo Hernani <hhernanni@gmail.com>
2020-08-01 16:12:20 -0300

    Add test for CommentsService

--- articles_api/src/articles-management/services/comments.service.spec.ts
+++ articles_api/src/articles-management/services/comments.service.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { CommentsService } from './comments.service';
+import { Comment } from '../entities';
+
+let mockCommentsRepository = {
+  save: (_: any) => {}
+}
+
+let commentInstance: Comment;
+const CommentsRepositoryProvider = getRepositoryToken(Comment);
 
 describe('CommentsService', () => {
   let service: CommentsService;
+  let commentsRepository: any;
+  let appModule: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentsService],
+    appModule = await Test.createTestingModule({
+      providers: [
+        CommentsService,
+        {
+          provide: CommentsRepositoryProvider,
+          useValue: mockCommentsRepository
+        }
+      ],
     }).compile();
 
-    service = module.get<CommentsService>(CommentsService);
+    service = await appModule.resolve<CommentsService>(CommentsService);
+    commentsRepository = appModule.get(CommentsRepositoryProvider);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('addComment', () => {
+    beforeEach(() => {
+      jest.spyOn(commentsRepository, 'save').mockImplementation(async () => commentInstance);
+    })
+
+    afterEach(() => {
+      jest.spyOn(commentsRepository, 'save').mockReset();
+    })
+
+    let commentAttrs = {
+      postId: 'uuid-sample',
+      content: 'Lorem ipsum'
+    }
+
+    it('should call commentsRepository to save comment into database', async () => {
+      await service.addComment(commentAttrs);
+      expect(commentsRepository.save).toHaveBeenCalledWith(commentAttrs);
+    });
   });
 });
