f4867a493845bf6c12cd76d93ca53da93cf13551
Hugo Hernani <hhernanni@gmail.com>
2020-08-01 16:20:49 -0300

    Add basic test structure for AuthorsService

--- articles_api/src/articles-management/services/authors.service.spec.ts
+++ articles_api/src/articles-management/services/authors.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { AuthorsService } from './authors.service';
+import { Author } from '../entities';
+
+let mockAuthorsRepository = {
+  findOne: (_: any) => {}
+}
+
+let authorInstance: Author;
+const AuthorsRepositoryProvider = getRepositoryToken(Author);
 
 describe('AuthorsService', () => {
   let service: AuthorsService;
+  let authorsRepository: any;
+  let appModule: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthorsService],
+    appModule = await Test.createTestingModule({
+      providers: [
+        AuthorsService,
+        {
+          provide: AuthorsRepositoryProvider,
+          useValue: mockAuthorsRepository
+        }
+      ],
     }).compile();
 
-    service = module.get<AuthorsService>(AuthorsService);
+    service = await appModule.resolve<AuthorsService>(AuthorsService);
+    authorsRepository = appModule.get(AuthorsRepositoryProvider);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('find', () => {
+    beforeEach(() => {
+      jest.spyOn(authorsRepository, 'findOne').mockImplementation(async () => authorInstance);
+    })
+
+    afterEach(() => {
+      jest.spyOn(authorsRepository, 'findOne').mockReset();
+    })
+
+    let authorId = 'uuid-sample';
+
+    it('should call authorsRepository to find author on database', async () => {
+      await service.find(authorId);
+      expect(authorsRepository.findOne).toHaveBeenCalledWith(authorId);
+    });
   });
 });
