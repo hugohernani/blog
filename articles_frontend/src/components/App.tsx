69dcbf9ed972b3dd4af940808cf3e119c98920c0
Hugo Hernani <hhernanni@gmail.com>
2020-08-26 03:08:49 -0300

    Add basic router mechanism

--- articles_frontend/src/components/App.tsx
+++ articles_frontend/src/components/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import '../styles/App.css';
 import { ApolloProvider } from '@apollo/client';
 import PostItems  from './PostItems';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from '../routes';
 
 const App = ({client}: {client: any}) => {
   return (
     <ApolloProvider client={client}>
-      <PostItems />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
     </ApolloProvider>
   );
 }
---
+++ articles_frontend/src/pages/Homepage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import PostItems from '../../components/PostItems';
+
+const Homepage: React.FC = () => {
+  return <PostItems />
+}
+
+export default Homepage;
