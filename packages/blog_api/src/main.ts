5d310388b86f0e51a2d8b556f7b97046522105ec
Hugo Hernani <hhernanni@gmail.com>
2020-09-03 20:25:49 -0300

    Make sure frontend apollo client will be able to connect with graphql server on nest through endpoint

--- packages/blog_api/src/main.ts
+++ packages/blog_api/src/main.ts
@@ -4,7 +4,12 @@ import { NestFactory } from '@nestjs/core'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // TODO: Apply more security
-  app.enableCors()
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      `http://blog_frontend:${process.env.BLOG_FRONTEND_APP_EXPOSED_PORT}`,
+    ],
+  })
 
   await app.listen(process.env.EXPOSED_PORT || 3001)
 }
--- packages/blog_frontend/src/index.tsx
+++ packages/blog_frontend/src/index.tsx
@@ -1,19 +1,20 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-import App from './App'
-import React from 'react'
-import ReactDOM from 'react-dom'
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const GRAPHQL_URL =`http://blog_api:${process.env.BLOG_API_APP_EXPOSED_PORT || 3001}/graphql`;
+// TODO: Add external domain to be access through blog_frontend in browser
+const GRAPHQL_URL = `http://localhost:${process.env.BLOG_API_APP_EXPOSED_PORT || 3001}/graphql`;
 
 const client = new ApolloClient({
   uri: GRAPHQL_URL,
   cache: new InMemoryCache(),
-})
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App client={client} />
   </React.StrictMode>,
   document.getElementById('root'),
-)
+);
