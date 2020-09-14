4d44ca604d12c4bfe8ee23e07a21a4814eea9e57
Hugo Hernani <hhernanni@gmail.com>
2020-09-14 19:28:44 -0300

    Add default sorting for posts read resolvers

--- packages/blog_api/src/articles-management/articles-management.module.ts
+++ packages/blog_api/src/articles-management/articles-management.module.ts
@@ -1,34 +1,51 @@
+import { AuthorDTO, CommentDTO, PostDTO } from './dto'
+import { AuthorEntity, CommentEntity, PostEntity } from './entities'
+import {
+  NestjsQueryGraphQLModule,
+  ReadResolverOpts,
+} from '@nestjs-query/query-graphql'
+import { SortDirection, SortField } from '@nestjs-query/core'
+
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { AuthorEntity, PostEntity, CommentEntity } from './entities'
-import { AuthorDTO, PostDTO, CommentDTO } from './dto';
-import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql'
 import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+const createdDefaultSort: SortField<PostEntity> = {
+  field: 'createdAt',
+  direction: SortDirection.DESC,
+}
+
+const defaultReadSort = { defaultSort: [createdDefaultSort] }
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      AuthorEntity, PostEntity, CommentEntity
-    ]),
+    TypeOrmModule.forFeature([AuthorEntity, PostEntity, CommentEntity]),
     NestjsQueryGraphQLModule.forFeature({
       imports: [
-        NestjsQueryTypeOrmModule.forFeature([AuthorEntity, PostEntity, CommentEntity])
+        NestjsQueryTypeOrmModule.forFeature([
+          AuthorEntity,
+          PostEntity,
+          CommentEntity,
+        ]),
       ],
       resolvers: [
         {
           DTOClass: AuthorDTO,
-          EntityClass: AuthorEntity
+          EntityClass: AuthorEntity,
         },
         {
           DTOClass: PostDTO,
-          EntityClass: PostEntity
+          EntityClass: PostEntity,
+          read: {
+            ...defaultReadSort,
+          } as ReadResolverOpts<unknown>,
         },
         {
           DTOClass: CommentDTO,
-          EntityClass: CommentEntity
-        }
-      ]
-    })
-  ]
+          EntityClass: CommentEntity,
+        },
+      ],
+    }),
+  ],
 })
 export class ArticlesManagementModule {}
