2ca58719803ec654548e0e29d045ae00ca0b7fc7
Hugo Hernani <hhernanni@gmail.com>
2020-09-21 16:15:18 -0300

    Remove unused nest controller/services and clean README [api]

--- packages/blog_api/src/app.module.ts
+++ packages/blog_api/src/app.module.ts
@@ -1,12 +1,8 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ArticlesManagementModule } from './articles-management/articles-management.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-// import { TruncateDirective } from './directives';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-// import { Upload } from './scalars/upload.scalar';
 
 @Module({
   imports: [
@@ -16,21 +12,10 @@ import { join } from 'path';
       debug: true,
       playground: true,
       installSubscriptionHandlers: true,
-      // uploads: {
-      //   maxFileSize: 20000000, // 20 MB
-      //   maxFiles: 5,
-      // },
-      // schemaDirectives: {
-      //   truncate: TruncateDirective
-      // }
     }),
     TypeOrmModule.forRoot(),
     ArticlesManagementModule,
   ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    // Upload
-  ],
+  providers: [],
 })
 export class AppModule {}
