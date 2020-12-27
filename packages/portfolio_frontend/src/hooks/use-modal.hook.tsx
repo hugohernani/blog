19b0a282cd0a784373c259512c18d70b093fdce5
Hugo Hernani <hhernanni@gmail.com>
2020-12-27 19:34:44 -0300

    Create basic useModal hook

---
+++ packages/portfolio_frontend/src/hooks/use-modal.hook.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ModalContext } from '../context';
+
+export const useModal = (): any => {
+  const context = React.useContext(ModalContext);
+  if (context == undefined) {
+    throw new Error('useModal must be used within a ModelProvider');
+  }
+  return context;
+};
