1c1d8981ecdf941cb95bc94fca65980bef480563
Hugo Hernani <hhernanni@gmail.com>
2021-03-08 19:17:49 -0300

    Updates

---
+++ precache-manifest.c7b0dbebd3da3bfcde06447d634e917b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63844c3d4f272ebef5b8c0fe154b126c",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "b707861249f424ae4640",
+    "url": "/blog/static/js/2.647dbb01.chunk.js"
+  },
+  {
+    "revision": "66e2e34f17a5dc6bd40b2dac27dc7a9b",
+    "url": "/blog/static/js/2.647dbb01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0144e6a01520464b0fdc",
+    "url": "/blog/static/js/main.0990b800.chunk.js"
+  },
+  {
+    "revision": "6b696e22fe2336877915",
+    "url": "/blog/static/js/runtime-main.43de64a3.js"
+  }
+]);
